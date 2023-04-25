c51cc38e405194ad418d0098b0597ade64f1a442
Dev-Franqqi <esendufranklinebi@gmail.com>
2023-04-25 02:36:09 +0100

    Added new text to home page

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className={inter.className}>
         
-    
+        <h1 className="text-white text-center mt-7 text-xl">Search for articles</h1>
 
     </main>
   )
